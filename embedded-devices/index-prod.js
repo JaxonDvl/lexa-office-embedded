4721c70f6fcac1e61c9b28805ac4ebf648679b42
JaxonDvl <tatualindvl@gmail.com>
2018-06-24 09:51:01 +0000

    updated socket setting

--- embedded-devices/index-prod.js
+++ embedded-devices/index-prod.js
@@ -7,76 +7,119 @@ socket.token = null;
 const deviceName = access_data["username"];
 const password = access_data["password"];
 
-socket.on('connect', function(){
+const sensor = require('node-dht-sensor');
+const SerialPort = require('serialport');
+
+const port = new SerialPort('/dev/ttyACM0', {
+    baudRate: 9600
+});
+
+const parsers = SerialPort.parsers;
+const parser = new parsers.Readline({
+    delimiter: '\r\n'
+});
+
+port.pipe(parser);
+
+
+socket.on('connect', function () {
     console.log("connected on client");
-    socket.emit('login', {deviceName, password});
+    socket.emit('login', { deviceName, password });
 });
 
-socket.on('event', function(data){
+socket.on('event', function (data) {
 
 });
 
-socket.on('login.success', function(data) {
+socket.on('login.success', function (data) {
     socket.token = data.token;
     console.log(data);
     socket.clocking = data.profile.clocking;
 })
 
-socket.on('change-clocking', function(data,ack) {
-    if(data.state === true) {
+socket.on('change-clocking', function (data, ack) {
+    if (data.state === true) {
         socket.clocking = true;
         console.log("REQUEST Change clock state")
-        ack({message: "Clock state is set to logging mode",
-            clocking:true});
+        ack({
+            message: "Clock state is set to logging mode",
+            clocking: true
+        });
     } else {
         socket.clocking = false;
         console.log("REQUEST Change clock state to false")
-        ack({message: "Clock state is set to register mode",
-        clocking:false});
+        ack({
+            message: "Clock state is set to register mode",
+            clocking: false
+        });
     }
 })
 
-socket.on('user.success', function(data) {
+socket.on('user.success', function (data) {
     console.log("fetch data")
     console.log(data);
-  
+
 })
-socket.on('client.error', function (data){
+socket.on('client.error', function (data) {
     console.log(data);
 })
-socket.on('error.messages', function(data){
+socket.on('error.messages', function (data) {
     console.log(data);
     console.log(socket.token);
 })
-socket.on("message", function(data){
+socket.on("message", function (data) {
     console.log("got message", data)
 })
 
-socket.on("cloud-message-temperature", function(data, ack){
+socket.on("cloud-message-temperature", function (data, ack) {
     console.log("REQUEST CM Temperature", data)
-    //here is the sensor dht
-    ack({temperature: "40"});
+    sensor.read(11, 4, function (err, temperature, humidity) {
+        if (!err) {
+            ack({ temperature: temperature.toFixed(1) });
+            console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+                'humidity: ' + humidity.toFixed(1) + '%'
+            );
+        }
+        else {
+            console.log(err);
+        }
+    });
 })
 
-socket.on("cloud-message-humidity", function(data, ack){
+socket.on("cloud-message-humidity", function (data, ack) {
     console.log("REQUEST CM Humidity", data)
-     //here is the sensor dht
-    ack({humidity: "20"});
+    sensor.read(11, 4, function (err, temperature, humidity) {
+        if (!err) {
+            ack({ humidity: humidity.toFixed(1) });
+            console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+                'humidity: ' + humidity.toFixed(1) + '%'
+            );
+        }
+        else {
+            console.log(err);
+        }
+    });
 })
-//this is information comming from the serial
-function intervalFunc() {
-    const tagId = "7675";
-    if(socket.clocking) {
+
+
+port.open(function () {
+    console.log("port is open")
+})
+
+parser.on('data', function (data) {
+
+    let tagId = data.toString("utf8").split(" ").join("");
+    console.log('Data:', tagId);
+    if (socket.clocking) {
         //clocking
-        socket.emit("data-log", {tagId});
+        socket.emit("data-log", { tagId });
     } else {
         //registration
-        socket.emit("data-tag", {tagId});
+        socket.emit("data-tag", { tagId });
     }
-    console.log("Hello!!!!");
-     }
+    console.log("TagID emited", tagId);
+})
 
-setInterval(intervalFunc,4000);
 
-socket.on('disconnect', function(){});
+socket.on('disconnect', function () { });
 
